277e3668b71f78854653cf32fe8070787fcf5bb0
alexandrelu1s <alexandrelu1s.ptn@gmail.com>
2023-10-09 14:37:52 -0300

    Repassando parametros de componentes pais para filhos

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -7,6 +7,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input()
+  gameCover: string =""
+  @Input()
+  gameLabel: string=""
+  @Input()
+  gameType: string ="Digital PS4"
+  @Input()
+  gamePrice: string ="399,90"
+
   constructor() { }
 
   ngOnInit(): void {
